3b8de881955eea4aafe0ef72bc8f71a6d62ffd63
Kevin Du <wedu@syr.edu>
2020-09-26 10:36:26 -0400

    Initial commit

---
+++ category-software/Format_String_Server/Files/server.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/ip.h>
+
+#define PORT 9090
+
+/* Changing this size will change the layout of the stack.
+ * We have added 2 dummy arrays: in main() and myprintf().
+ * Instructors can change this value each year, so students 
+ * won't be able to use the solutions from the past.   
+ * Suggested value: between 0 and 300  */
+#ifndef DUMMY_SIZE
+#define DUMMY_SIZE 100
+#endif
+
+char *secret = "A secret message\n";
+unsigned int  target = 0x11223344;
+
+void myprintf(char *msg)
+{
+    uintptr_t framep;
+    // Copy the ebp value into framep, and print it out
+    asm("movl %%ebp, %0" : "=r"(framep));
+    printf("The ebp value inside myprintf() is: 0x%.8x\n", framep);
+
+    /* Change the size of the dummy array to randomize the parameters 
+       for this lab. Need to use the array at least once */
+    char dummy[DUMMY_SIZE];  memset(dummy, 0, DUMMY_SIZE); 
+
+    // This line has a format-string vulnerability
+    printf(msg);
+    printf("The value of the 'target' variable (after): 0x%.8x\n", target);
+}
+
+/* This function provides some helpful information. It is meant to
+ *   simplify the lab tasks. In practice, attackers need to figure
+ *   out the information by themselves. */
+void helper()
+{
+    printf("The address of the secret: 0x%.8x\n", (unsigned) secret);
+    printf("The address of the 'target' variable: 0x%.8x\n", 
+            (unsigned) &target);
+    printf("The value of the 'target' variable (before): 0x%.8x\n", target);
+}
+
+void main()
+{
+    struct sockaddr_in server;
+    struct sockaddr_in client;
+    int clientLen;
+    char buf[1500];
+
+    /* Change the size of the dummy array to randomize the parameters 
+       for this lab. Need to use the array at least once */
+    char dummy[DUMMY_SIZE];  memset(dummy, 0, DUMMY_SIZE); 
+
+    printf("The address of the input array: 0x%.8x\n", (unsigned) buf);
+     
+    helper();
+
+    int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    memset((char *) &server, 0, sizeof(server));
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = htonl(INADDR_ANY);
+    server.sin_port = htons(PORT);
+
+    if (bind(sock, (struct sockaddr *) &server, sizeof(server)) < 0)
+        perror("ERROR on binding");
+
+    while (1) {
+        bzero(buf, 1500);
+        recvfrom(sock, buf, 1500-1, 0,
+                 (struct sockaddr *) &client, &clientLen);
+        myprintf(buf);
+    }
+    close(sock);
+}
