319fb5684f34fe2eb638475e177ca556f55940b0
Kevin Du <seed@Ubuntu 20.04 VM>
2020-11-03 09:51:29 -0500

    moved the folder to another place

--- common-files/docker_images/dns/ns_attacker/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Start the nameserver
-service named start
-
-tail -f /dev/null
--- common-files/docker_images/dns/user/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Copy the prepared resolv.conf file
-cp /etc/resolv.conf.override /etc/resolv.conf
-
-# Start the shell
-/bin/bash
-
