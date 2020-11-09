65bfde5f785ea89d56a4dafc60eb2eb73f272bf3
Kevin Du <seed@Ubuntu 20.04 VM>
2020-11-08 23:39:56 -0500

    added lab setup files

---
+++ category-web/Web_XSS_Elgg/Labsetup/image_www/elgg/dropdown.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Elgg dropdown display
+ * Displays a value that was entered into the system via a dropdown
+ *
+ * @uses $vars['text'] The text to display
+ */
+
+// echo htmlspecialchars($vars['value'], ENT_QUOTES, 'UTF-8', false);
+echo $vars['value'];
---
+++ category-web/Web_XSS_Elgg/Labsetup/image_www/start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+service apache2 start
+tail -f /dev/null
