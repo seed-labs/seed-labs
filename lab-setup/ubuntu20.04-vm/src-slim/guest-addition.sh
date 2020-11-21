0152315133225398d7fb333c4efc37c53946ea2f
Kevin Du <wedu@syr.edu>
2020-11-21 13:35:30 -0500

    replace some files with symbolic links to avoid duplication

--- lab-setup/ubuntu20.04-vm/src-slim/guest-addition.sh
+++ lab-setup/ubuntu20.04-vm/src-slim/guest-addition.sh
@@ -1,7 +1,1 @@
-#!/bin/bash 
-
-echo "Installing VirtualBox Guest Addtion ..."
-
-sudo add-apt-repository multiverse
-sudo apt -y install virtualbox-guest-dkms virtualbox-guest-x11
-
+../src/guest-addition.sh
--- lab-setup/ubuntu20.04-vm/src-slim/misc.sh
+++ lab-setup/ubuntu20.04-vm/src-slim/misc.sh
@@ -1,13 +1,1 @@
-#!/bin/bash
-
-echo "Installing miscellaneous tools ..."
-DOWNLOADS=/home/seed/Downloads
-
-
-# Install gdbpeda (gdb plugin)
-git clone https://github.com/longld/peda.git $DOWNLOADS/gdbpeda
-sudo cp -r $DOWNLOADS/gdbpeda  /opt
-echo "source /opt/gdbpeda/peda.py" >> ~/.gdbinit
-rm -rf $DOWNLOADS/gdbpeda
-
-
+../src/misc.sh
