3bc3229c5601bf9793af36adf8dae357c05b7a77
Kevin Du <wedu@syr.edu>
2021-11-07 08:42:19 -0500

    added a program to calculate the frequency of n-grams

---
+++ category-crypto/Crypto_Encryption/Labsetup/Files/freq.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+from collections import Counter
+import re
+
+TOP_K  = 20
+N_GRAM = 3
+
+# Generate all the n-grams for value n
+def ngrams(n, text):
+    for i in range(len(text) -n + 1):
+        # Ignore n-grams containing white space
+        if not re.search(r'\s', text[i:i+n]):
+           yield text[i:i+n]
+
+# Read the data from the ciphertext
+with open('ciphertext.txt') as f:
+    text = f.read()
+
+# Count, sort, and print out the n-grams
+for N in range(N_GRAM):
+   print("-------------------------------------")
+   print("{}-gram (top {}):".format(N+1, TOP_K))
+   counts = Counter(ngrams(N+1, text))        # Count
+   sorted_counts = counts.most_common(TOP_K)  # Sort 
+   for ngram, count in sorted_counts:                  
+       print("{}: {}".format(ngram, count))   # Print
