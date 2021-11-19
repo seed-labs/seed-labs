09efbcc2be7dc5bb00a008e8f31f56f309600c35
Kevin Du <wedu@syr.edu>
2021-11-19 17:52:25 -0500

    ...

--- category-network/DNS_Infrastructure/Labsetup/component-dns.py
+++ category-network/DNS_Infrastructure/Labsetup/component-dns.py
@@ -11,36 +11,31 @@ emu = Emulator()
 dns = DomainNameService(autoNameServer = False)
 
 # Create two nameservers for the root zone
-dns.install('a-root-server').addZone('.').setMaster()   # Master server
-dns.install('b-root-server').addZone('.')               # Slave server
+dns.install('root-server-a').addZone('.').setMaster()   # Master server
+dns.install('root-server-b').addZone('.')               # Slave server
 
 # Create nameservers for TLD and ccTLD zones
-dns.install('a-com-server').addZone('com.').setMaster()  
-dns.install('b-com-server').addZone('com.')  
-dns.install('net-server').addZone('net.')
+dns.install('com-server-a').addZone('com.').setMaster()  
+dns.install('com-server-b').addZone('com.')  
 dns.install('edu-server').addZone('edu.')
-dns.install('edu-server').addZone('xyz.')
 
 # Create nameservers for second-level zones
-dns.install('ns-google-com').addZone('google.com.')
-dns.install('ns-example-net').addZone('example.net.')
-dns.install('ns-syr-edu').addZone('syr.edu.')
+dns.install('ns-example-com').addZone('example.com.')
 
-# Add records to zones 
-dns.getZone('google.com.').addRecord('@ A 2.2.2.2') 
-dns.getZone('example.net.').addRecord('@ A 3.3.3.3') 
-dns.getZone('syr.edu.').addRecord('@ A 128.230.18.63') 
+# Create two place holders
+dns.install('ns-AAAAA').addZone('AAAAA.')
+dns.install('ns-BBBBB').addZone('BBBBB.')
+
 
 # Customize the display names (for visualization purpose)
-emu.getVirtualNode('a-root-server').setDisplayName('Root-A')
-emu.getVirtualNode('b-root-server').setDisplayName('Root-B')
-emu.getVirtualNode('a-com-server').setDisplayName('COM-A')
-emu.getVirtualNode('b-com-server').setDisplayName('COM-B')
-emu.getVirtualNode('net-server').setDisplayName('NET')
-emu.getVirtualNode('edu-server').setDisplayName('EDU')
-emu.getVirtualNode('ns-google-com').setDisplayName('google.com')
-emu.getVirtualNode('ns-example-net').setDisplayName('example.net')
-emu.getVirtualNode('ns-syr-edu').setDisplayName('syr.edu')
+emu.getVirtualNode('root-server-a').setDisplayName('DNS-Root-A')
+emu.getVirtualNode('root-server-b').setDisplayName('DNS-Root-B')
+emu.getVirtualNode('com-server-a').setDisplayName('DNS-COM-A')
+emu.getVirtualNode('com-server-b').setDisplayName('DNS-COM-B')
+emu.getVirtualNode('edu-server').setDisplayName('DNS-EDU')
+emu.getVirtualNode('ns-example-com').setDisplayName('DNS-Example.com')
+emu.getVirtualNode('ns-AAAAA').setDisplayName('DNS-AAAAA')
+emu.getVirtualNode('ns-BBBBB').setDisplayName('DNS-BBBBB')
 
 
 ###########################################################
--- category-network/DNS_Infrastructure/Labsetup/internet-emulator.py
+++ category-network/DNS_Infrastructure/Labsetup/internet-emulator.py
@@ -26,15 +26,14 @@ else:
 # Action.FIRST will look for the first acceptable node that satisfies the filter rule.
 # There are several other filters types that are not shown in this example.
 
-emu.addBinding(Binding('a-root-server',  filter=Filter(asn=171), action=Action.FIRST))
-emu.addBinding(Binding('b-root-server',  filter=Filter(asn=150), action=Action.FIRST))
-emu.addBinding(Binding('a-com-server',   filter=Filter(asn=151), action=Action.FIRST))
-emu.addBinding(Binding('b-com-server',   filter=Filter(asn=152), action=Action.FIRST))
-emu.addBinding(Binding('net-server',   filter=Filter(asn=152), action=Action.FIRST))
-emu.addBinding(Binding('edu-server',   filter=Filter(asn=153), action=Action.FIRST))
-emu.addBinding(Binding('ns-google-com',  filter=Filter(asn=162), action=Action.FIRST))
-emu.addBinding(Binding('ns-example-net', filter=Filter(asn=163), action=Action.FIRST))
-emu.addBinding(Binding('ns-syr-edu',     filter=Filter(asn=164), action=Action.FIRST))
+emu.addBinding(Binding('root-server-a',  filter=Filter(asn=150), action=Action.FIRST))
+emu.addBinding(Binding('root-server-b',  filter=Filter(asn=160), action=Action.FIRST))
+emu.addBinding(Binding('com-server-a',   filter=Filter(asn=151), action=Action.FIRST))
+emu.addBinding(Binding('com-server-b',   filter=Filter(asn=161), action=Action.FIRST))
+emu.addBinding(Binding('edu-server',     filter=Filter(asn=152), action=Action.FIRST))
+emu.addBinding(Binding('ns-example-com', filter=Filter(asn=154), action=Action.FIRST))
+emu.addBinding(Binding('ns-AAAAA',     filter=Filter(asn=162), action=Action.FIRST))
+emu.addBinding(Binding('ns-BBBBB',   filter=Filter(asn=170), action=Action.FIRST))
 
 #####################################################################################
 # Create two local DNS servers (virtual nodes).
@@ -49,21 +48,24 @@ emu.getVirtualNode('global-dns-2').setDisplayName('Global DNS-2')
 # Create two new host in AS-152 and AS-153, use them to host the local DNS server.
 # We can also host it on an existing node.
 base: Base = emu.getLayer('Base')
-as152 = base.getAutonomousSystem(152)
-as152.createHost('local-dns-1').joinNetwork('net0', address = '10.152.0.53')
 as153 = base.getAutonomousSystem(153)
-as153.createHost('local-dns-2').joinNetwork('net0', address = '10.153.0.53')
+as153.createHost('local-dns-1').joinNetwork('net0', address = '10.153.0.53')
+as163 = base.getAutonomousSystem(163)
+as163.createHost('local-dns-2').joinNetwork('net0', address = '10.163.0.53')
 
 # Bind the Local DNS virtual nodes to physical nodes
-emu.addBinding(Binding('global-dns-1', filter = Filter(asn=152, nodeName="local-dns-1")))
-emu.addBinding(Binding('global-dns-2', filter = Filter(asn=153, nodeName="local-dns-2")))
+emu.addBinding(Binding('global-dns-1', filter = Filter(asn=153, nodeName="local-dns-1")))
+emu.addBinding(Binding('global-dns-2', filter = Filter(asn=163, nodeName="local-dns-2")))
 
-# Add 10.152.0.53 as the local DNS server for AS-160 and AS-170
-# Add 10.153.0.53 as the local DNS server for all the other nodes
-# We can also set this for individual nodes
-base.getAutonomousSystem(160).setNameServers(['10.152.0.53'])
-base.getAutonomousSystem(170).setNameServers(['10.152.0.53'])
-base.setNameServers(['10.153.0.53'])
+# Use the following to set the local DNS for all ASes
+# base.setNameServers(['10.163.0.53'])  
+
+
+# We choose to set the local DNS for individual nodes
+# We skip 155 and 164, so students can work on that.
+asnlist = [150, 151, 152, 153, 154, 156, 160, 161, 162, 163, 170, 171, 190]
+for asn in asnlist: 
+    base.getAutonomousSystem(asn).setNameServers(['10.153.0.53'])
 
 # Add the ldns layer
 emu.addLayer(ldns)
