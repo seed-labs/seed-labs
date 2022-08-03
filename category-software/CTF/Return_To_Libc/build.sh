31ebda40b9763f3ba2d4a2cb6c1ea770d15c61cc
simmons-wustl <dylan.simmons@wustl.edu>
2022-08-03 01:51:52 +0000

    Got SSh server running on Ubuntu container.

--- category-software/CTF/Return_To_Libc/build.sh
+++ category-software/CTF/Return_To_Libc/build.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-echo "Disabling Address Space Layout Randomization (ASLR) on the host so that it'll be disabled in the container..."
-sudo sysctl -w kernel.randomize_va_space=0
-ASLR=$(sudo cat /proc/sys/kernel/randomize_va_space)
-if [ $ASLR -eq 0 ]
+ASLR=$(cat /proc/sys/kernel/randomize_va_space)
+if [ $ASLR -ne 0 ]
 then
-   echo "ASLR has been disabled"
-else
-   echo "ASLR was NOT disabled! Manually disable it via: 'sudo sysctl -w kernel.randomize_va_space=0'"
+   echo "Disabling Address Space Layout Randomization (ASLR) on the host so that it'll be disabled in the container..."
+   sudo sysctl -w kernel.randomize_va_space=0
+   ASLR=$(cat /proc/sys/kernel/randomize_va_space)
+   if [ $ASLR -eq 0 ]
+   then
+      echo "ASLR has been disabled"
+   else
+      echo "ASLR was NOT disabled! Manually disable it via: 'sudo sysctl -w kernel.randomize_va_space=0'"
+   fi
 fi
+
+sudo docker-compose build
