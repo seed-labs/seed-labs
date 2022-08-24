6b9e2d65f8b0bf5862ef9fa1c162f642ce9f5ef1
simmons-wustl <dylan.simmons@wustl.edu>
2022-08-24 01:01:06 +0000

    Added warning messages about disabling ASLR to start and stop scripts.

--- category-software/CTF/Return_To_Libc/start.sh
+++ category-software/CTF/Return_To_Libc/start.sh
@@ -1,24 +1,43 @@
 #!/bin/bash
 
 # if ASLR is disabled on host, then it will be disabled in containers
+sudo sysctl -w kernel.randomize_va_space=0
+
 ASLR=$(cat /proc/sys/kernel/randomize_va_space)
 
 if [ $ASLR -eq 0 ]
 then
-   echo "ASLR is disabled, starting up containers..." 
-   sudo docker-compose up -d
-   sudo docker ps -f name=return_to_libc_
+   echo "  █████   ███   █████   █████████   ███████████   ██████   █████ █████ ██████   █████   █████████  ███"
+   echo " ░░███   ░███  ░░███   ███░░░░░███ ░░███░░░░░███ ░░██████ ░░███ ░░███ ░░██████ ░░███   ███░░░░░███░███"
+   echo "  ░███   ░███   ░███  ░███    ░███  ░███    ░███  ░███░███ ░███  ░███  ░███░███ ░███  ███     ░░░ ░███"
+   echo "  ░███   ░███   ░███  ░███████████  ░██████████   ░███░░███░███  ░███  ░███░░███░███ ░███         ░███"
+   echo "  ░░███  █████  ███   ░███░░░░░███  ░███░░░░░███  ░███ ░░██████  ░███  ░███ ░░██████ ░███    █████░███"
+   echo "   ░░░█████░█████░    ░███    ░███  ░███    ░███  ░███  ░░█████  ░███  ░███  ░░█████ ░░███  ░░███ ░░░ "
+   echo "     ░░███ ░░███      █████   █████ █████   █████ █████  ░░█████ █████ █████  ░░█████ ░░█████████  ███"
+   echo "      ░░░   ░░░      ░░░░░   ░░░░░ ░░░░░   ░░░░░ ░░░░░    ░░░░░ ░░░░░ ░░░░░    ░░░░░   ░░░░░░░░░  ░░░ "
+   echo ""
+   echo "ADDRESS SPACE LAYOUT RANDOMIZATION (ASLR) HAS BEEN DISABLED ON THE HOST SO THAT IT'LL BE DISABLED IN THE CONTAINER."
+   echo "THIS IS A SERIOUS SECURITY VULNERABILITY THAT WILL PERSIST BEYOND RUNNING THIS CHALLENGE!"
+   echo ""
+   echo "DO YOU UNDERSTAND THAT YOUR HOST SYSTEM HAS BEEN MADE VULNERABLE IN ORDER TO RUN THIS CHALLENGE,"
+   while true; do
+      read -p "AND DO YOU AGREE TO ENABLE ASLR ON YOUR HOST WHEN THIS CHALLENGE IS COMPLETED? [Y|N] " yn
+      case $yn in
+         [Yy]* )
+            echo "Starting up containers..."
+            sudo docker-compose up -d
+            sudo docker ps -f name=return_to_libc_
+            break
+            ;;
+         [Nn]* )
+            echo "ASLR MUST BE DISABLED ON THE HOST SO THAT IT WILL BE DISABLED IN THE CONTAINERS..."
+            exit
+            ;;
+         * )
+            echo "Please answer yes or no!"
+            ;;
+      esac
+   done
 else
-   echo "Disabling Address Space Layout Randomization (ASLR) on the host so that it'll be disabled in the container..."
-   sudo sysctl -w kernel.randomize_va_space=0
-   ASLR=$(cat /proc/sys/kernel/randomize_va_space)
-   if [ $ASLR -eq 0 ]
-   then
-      echo "ASLR is disabled, starting up containers..." 
-      sudo docker-compose up -d
-      sudo docker ps -f name=return_to_libc_
-   else
-      echo "ASLR was NOT disabled! Manually disable it before running containers: 'sudo sysctl -w kernel.randomize_va_space=0'"
-   fi
+   echo "ASLR was NOT disabled! Manually disable it before running this script again: 'sudo sysctl -w kernel.randomize_va_space=0'"
 fi
-
--- category-software/CTF/Return_To_Libc/stop.sh
+++ category-software/CTF/Return_To_Libc/stop.sh
@@ -1,3 +1,31 @@
 #!/bin/bash
 
-sudo docker stop $(sudo docker ps -f name=return_to_libc_ -q)
+if [ -n "$(sudo docker ps -f name=return_to_libc_ -q)" ]
+then
+   sudo docker stop $(sudo docker ps -f name=return_to_libc_ -q)
+   echo ""
+fi
+
+echo "ENABLING ASLR..."
+sudo sysctl -w kernel.randomize_va_space=2
+
+ASLR=$(cat /proc/sys/kernel/randomize_va_space)
+
+if [ $ASLR -ne 2 ]
+then
+   echo "  █████   ███   █████   █████████   ███████████   ██████   █████ █████ ██████   █████   █████████  ███"
+   echo " ░░███   ░███  ░░███   ███░░░░░███ ░░███░░░░░███ ░░██████ ░░███ ░░███ ░░██████ ░░███   ███░░░░░███░███"
+   echo "  ░███   ░███   ░███  ░███    ░███  ░███    ░███  ░███░███ ░███  ░███  ░███░███ ░███  ███     ░░░ ░███"
+   echo "  ░███   ░███   ░███  ░███████████  ░██████████   ░███░░███░███  ░███  ░███░░███░███ ░███         ░███"
+   echo "  ░░███  █████  ███   ░███░░░░░███  ░███░░░░░███  ░███ ░░██████  ░███  ░███ ░░██████ ░███    █████░███"
+   echo "   ░░░█████░█████░    ░███    ░███  ░███    ░███  ░███  ░░█████  ░███  ░███  ░░█████ ░░███  ░░███ ░░░ "
+   echo "     ░░███ ░░███      █████   █████ █████   █████ █████  ░░█████ █████ █████  ░░█████ ░░█████████  ███"
+   echo "      ░░░   ░░░      ░░░░░   ░░░░░ ░░░░░   ░░░░░ ░░░░░    ░░░░░ ░░░░░ ░░░░░    ░░░░░   ░░░░░░░░░  ░░░ "
+   echo ""
+   echo "ADDRESS SPACE LAYOUT RANDOMIZATION (ASLR) IS DISABLED ON THE HOST."
+   echo "THIS IS A SERIOUS SECURITY VULNERABILITY THAT WILL PERSIST BEYOND RUNNING THIS CHALLENGE!"
+   echo ""
+   echo "Enable ASLR via 'sudo sysctl -w kernel.randomize_va_space=2'"
+else
+   echo "ASLR is enabled, your host system is safe again."
+fi
