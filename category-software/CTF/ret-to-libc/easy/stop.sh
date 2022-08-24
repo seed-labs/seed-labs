135db3a976676e3e593f1dbce1eea015371f9142
simmons-wustl <dylan.simmons@wustl.edu>
2022-08-24 21:30:39 +0000

    Copied ret-to-libc challenge files into an easy and difficult directory.

---
+++ category-software/CTF/ret-to-libc/easy/stop.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+if [ -n "$(sudo docker ps -f name=return_to_libc_ -q)" ]
+then
+   echo "STOPPING CONTAINERS..."
+   echo
+   sudo docker stop $(sudo docker ps -f name=return_to_libc_ -q)
+   echo 
+fi
+
+echo "ENABLING ASLR..."
+echo
+sudo sysctl -w kernel.randomize_va_space=2
+
+ASLR=$(cat /proc/sys/kernel/randomize_va_space)
+
+if [ $ASLR -ne 2 ]
+then
+   echo
+   echo "  █████   ███   █████   █████████   ███████████   ██████   █████ █████ ██████   █████   █████████  ███"
+   echo " ░░███   ░███  ░░███   ███░░░░░███ ░░███░░░░░███ ░░██████ ░░███ ░░███ ░░██████ ░░███   ███░░░░░███░███"
+   echo "  ░███   ░███   ░███  ░███    ░███  ░███    ░███  ░███░███ ░███  ░███  ░███░███ ░███  ███     ░░░ ░███"
+   echo "  ░███   ░███   ░███  ░███████████  ░██████████   ░███░░███░███  ░███  ░███░░███░███ ░███         ░███"
+   echo "  ░░███  █████  ███   ░███░░░░░███  ░███░░░░░███  ░███ ░░██████  ░███  ░███ ░░██████ ░███    █████░███"
+   echo "   ░░░█████░█████░    ░███    ░███  ░███    ░███  ░███  ░░█████  ░███  ░███  ░░█████ ░░███  ░░███ ░░░ "
+   echo "     ░░███ ░░███      █████   █████ █████   █████ █████  ░░█████ █████ █████  ░░█████ ░░█████████  ███"
+   echo "      ░░░   ░░░      ░░░░░   ░░░░░ ░░░░░   ░░░░░ ░░░░░    ░░░░░ ░░░░░ ░░░░░    ░░░░░   ░░░░░░░░░  ░░░ "
+   echo 
+   echo "ADDRESS SPACE LAYOUT RANDOMIZATION (ASLR) IS DISABLED ON THE HOST."
+   echo "THIS IS A SERIOUS SECURITY VULNERABILITY THAT WILL PERSIST BEYOND RUNNING THIS CHALLENGE!"
+   echo 
+   echo "ENABLE ASLR VIA 'sudo sysctl -w kernel.randomize_va_space=2'"
+else
+   echo
+   echo "ASLR IS ENABLED, YOUR HOST SYSTEM IS SAFE AGAIN!"
+fi
