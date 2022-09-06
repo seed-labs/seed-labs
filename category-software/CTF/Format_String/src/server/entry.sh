0b28e540305819f66afb6a7f3a0fce2d37b7f039
simmons-wustl <dylan.simmons@wustl.edu>
2022-09-06 00:26:07 +0000

    Made buffer size in vulnerable program configurable.

--- category-software/CTF/Format_String/src/server/entry.sh
+++ category-software/CTF/Format_String/src/server/entry.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-RANDOM=$(date +%s%N) # seed random
+# Tells xinetd to stay in the foreground rather than detaching itself, so that Docker container will stay alive.
+xinetd -dontfork
 
-MAX=800
-MIN=100
-SIZE=$(( $RANDOM % $MAX + $MIN )) # generate random number between MIN and MAX 
-
-while [[ $(( $SIZE % 8 )) -ne 0 && $SIZE -lt $MAX ]]
-do
-   ((SIZE++))
-done
-
-gcc -static -m32 -z execstack -DBUF_SIZE=${SIZE} /ctf/format.c -o /ctf/format
-
-rm /ctf/format.c
-
-xinetd -dontfork # Tells xinetd to stay in the foreground rather than detaching itself, so that Docker container will stay alive.
