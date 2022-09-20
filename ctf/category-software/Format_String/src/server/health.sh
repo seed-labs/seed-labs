59fc22295f7328af19ddad7ccf6e1a42219100b7
simmons-wustl <dylan.simmons@wustl.edu>
2022-09-20 01:31:18 +0000

    Added new script for checking running health status of containers.

--- ctf/category-software/Format_String/src/server/health.sh
+++ ctf/category-software/Format_String/src/server/health.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# if ASLR is disabled on host, then it will be disabled in containers
 ASLR=$(cat /proc/sys/kernel/randomize_va_space)
 
 if [ "$ASLR" -ne 0 ];
@@ -8,9 +7,17 @@ then
    # if ASLR is not disabled in container we should return before doing more,
    # which will cause the container to die and indicate that there's
    # something wrong
-   exit 1
+   exit 1 # fail
 fi
 
-# Tells xinetd to stay in the foreground rather than detaching itself, so that Docker container will stay alive.
-xinetd -dontfork
+# See if port is open for TCP connection, 1 second timeout
+nc -z -v -w 1 localhost 9999 &> /dev/null
 
+CONN=$?
+
+if [ "$CONN" != 0 ];
+then
+   exit 1 # fail
+fi
+
+exit 0 # success
