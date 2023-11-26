8e963bf10caa85805b47bdab13c940fcfe6f91a9
Kevin Du <wedu@syr.edu>
2023-11-26 15:04:42 -0500

    the map folder is no longer needed

--- category-network/Morris_Worm/Labsetup/map/frontend/src/common/console-event.ts
+++
@@ -1,5 +0,0 @@
-export interface ConsoleEvent{
-    type: 'ready' | 'closed' | 'error' | 'focus' | 'blur' | 'data' | 'rawkey';
-    id: string;
-    data?: any;
-}
--- category-network/Morris_Worm/Labsetup/map/start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd /usr/src/app/backend
-while true; do node ./bin/main.js; done
