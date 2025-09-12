f03154efdab2c8beeadde7a4d07988ff0d306a22
kKaiFang <1171367990@qq.com>
2025-09-12 18:01:42 +0800

    update untu24.04-vm

---
+++ lab-setup/ubuntu24.04-vm/src-cloud/install_docker.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+set -e
+USERID=seed
+#====================================
+echo "==================================="
+echo "Installing Docker Utilities..."
+# Uninstall old versions
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+
+# Set up Docker's apt repository
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get -y install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+# Install docker
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Start docker and enable it to start after the system reboot:
+sudo systemctl enable --now docker
+
+sudo groupadd docker
+
+sudo gpasswd -a $USERID docker
+
+sudo newgrp docker
