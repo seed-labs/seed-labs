a6d5560b0997bb9dee0218149544317ea992e83b
kKaiFang <1171367990@qq.com>
2025-10-16 18:58:05 +0800

    del todofiles

--- lab-setup/ubuntu24.04-vm/src-cloud&desktop/Files/VNC/websockify/docker/build.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-set -e -x
-cd "$(dirname "$0")"
-(cd .. && python3 setup.py sdist --dist-dir docker/)
-docker build -t novnc/websockify .
--- lab-setup/ubuntu24.04-vm/src-cloud&desktop/Files/VNC/websockify/websockify.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-set -e
-cd "$(dirname "$0")"
-exec python3 -m websockify "$@"
